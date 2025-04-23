27677dedd72dac37b5ea252900a61b882e599980
HIRWA Jean Eric <hirwaminerve25@gmail.com>
2025-04-23 18:59:44 +0200

    application of env variables

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,23 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC5gFydqU2hcxqfYM-2SAR1l0-ikafeKMQ",
-  authDomain: "shecancode-igirerwanda.firebaseapp.com",
-  projectId: "shecancode-igirerwanda",
-  storageBucket: "shecancode-igirerwanda.firebasestorage.app",
-  messagingSenderId: "950284944017",
-  appId: "1:950284944017:web:00f3d5458f16b779a0b530",
-  measurementId: "G-TN0CPVNP93",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
-// const firebaseConfig = {
-//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-//   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
